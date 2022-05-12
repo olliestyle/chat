326ea01d56d2abd1825231efbde2fce650b70f58
olliestyle <olliestyle1989@gmail.com>
2022-05-12 21:44:49 +0300

    6. ResponseEntity [#504799]

--- src/main/java/ru/job4j/chat/controller/ChatController.java
+++ src/main/java/ru/job4j/chat/controller/ChatController.java
@@ -1,6 +1,8 @@
 package ru.job4j.chat.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.chat.model.Message;
@@ -26,43 +28,52 @@ public class ChatController {
     }
 
     @GetMapping("/room/{roomId}/message")
-    public List<Message> findAllMessagesByRoomId(@PathVariable int roomId) {
+    public ResponseEntity<List<Message>> findAllMessagesByRoomId(@PathVariable int roomId) {
         if (roomService.findById(roomId).isEmpty()) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Room not found."
             );
         }
-        return chatService.findAllMessagesByRoomId(roomId);
+        return new ResponseEntity<>(
+                chatService.findAllMessagesByRoomId(roomId),
+                HttpStatus.OK);
     }
 
     @GetMapping("/room/{roomId}/person")
-    public List<Person> findAllPersonsByRoomId(@PathVariable int roomId) {
+    public ResponseEntity<List<Person>> findAllPersonsByRoomId(@PathVariable int roomId) {
         if (roomService.findById(roomId).isEmpty()) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Room not found."
             );
         }
-        return chatService.findAllPersonsByRoomId(roomId);
+        var body = chatService.findAllPersonsByRoomId(roomId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
     }
 
     @GetMapping("/room/{roomId}/person/{personId}/message")
-    public List<Message> findAllMessagesByRoomIdAndPersonId(@PathVariable int roomId, @PathVariable int personId) {
+    public ResponseEntity<List<Message>> findAllMessagesByRoomIdAndPersonId(@PathVariable int roomId, @PathVariable int personId) {
         if (roomService.findById(roomId).isEmpty() || personService.findById(personId).isEmpty()) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Room or Person not found."
             );
         }
-        return chatService.findAllMessagesByRoomIdAndPersonId(roomId, personId);
+        return new ResponseEntity<>(
+                chatService.findAllMessagesByRoomIdAndPersonId(roomId, personId),
+                HttpStatus.OK
+                );
     }
 
     @GetMapping("/person/{personId}/message")
-    public List<Message> findAllMessagesByPersonId(@PathVariable int personId) {
+    public ResponseEntity<List<Message>> findAllMessagesByPersonId(@PathVariable int personId) {
         if (personService.findById(personId).isEmpty()) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Person not found."
             );
         }
-        return chatService.findAllMessagesByPersonId(personId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(chatService.findAllMessagesByPersonId(personId));
     }
 
 }
